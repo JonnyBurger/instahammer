812ef803a3ecad32b9393e6531a894a766dfab38
Lukas Zirngibl <lukas.e.zirngibl@gmail.com>
2019-04-06 16:36:25 +0200

    stable

--- src/colors.ts
+++ src/colors.ts
@@ -1,8 +1,6 @@
+import { darken } from 'polished'
 
-
-import {darken} from 'polished';
-
-export const SULZER = '#1D59AF';
-export const WINTI = '#EA0001';
-export const GRAY = darken(0.07, '#fff');
-
+export const SULZER = '#1D59AF'
+export const WINTI = '#EA0001'
+export const GRAY = darken(0.07, '#fff')
+export const BACKGROUND = '#F8F8F8'
