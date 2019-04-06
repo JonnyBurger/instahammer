1073c1acde22ea4b4eadd6020689913372919412
Jonny Burger <jonathanburger11@gmail.com>
2019-04-06 17:32:13 +0200

    ¨

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import {View, TextInput} from 'react-native';
-import styled from 'styled-components';
-import {SULZER} from './colors';
-import {LitAnimation, Rotating} from './LitAnimation';
-import {Button} from './Button';
+import React from 'react'
+import { View, TextInput } from 'react-native'
+import styled from 'styled-components'
+import { SULZER } from './colors'
+import { LitAnimation, Rotating } from './LitAnimation'
+import { Button } from './Button'
 
 const Title = styled(TextInput)`
-	font-size: 30px;
-	font-family: 'frutiger-bold';
-`;
+  font-size: 30px;
+  font-family: 'frutiger-bold';
+`
 
 const Description = styled(TextInput)`
-	font-family: 'frutiger';
-	font-size: 20px;
-	height: 100px;
-`;
+  font-family: 'frutiger';
+  font-size: 20px;
+  height: 100px;
+`
 export class Form extends React.Component {
-	render() {
-		return (
-			<View style={{padding: 30}}>
-				<View style={{height: 25}} />
-				<Title placeholder="Enter title" />
-				<View style={{height: 10}} />
-				<Description placeholder="Describe your inquiry" multiline />
-				<View style={{height: 25}} />
-				<LitAnimation delay={0} type="time" />
-				<View style={{height: 10}} />
-				<LitAnimation delay={500} type="location" />
-				<View style={{height: 20}} />
-				<Button />
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View style={{ padding: 30 }}>
+        <View style={{ height: 25 }} />
+        <Title placeholder="Enter title" />
+        <View style={{ height: 10 }} />
+        <Description placeholder="Describe your inquiry" multiline />
+        <View style={{ height: 25 }} />
+        <LitAnimation delay={0} type="time" />
+        <View style={{ height: 10 }} />
+        <LitAnimation delay={500} type="location" />
+        <View style={{ height: 20 }} />
+        <Button label="Create report" />
+      </View>
+    )
+  }
 }
