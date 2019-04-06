7fec2c1d401479578dae3a309507b15e78bd76ea
Jonny Burger <jonathanburger11@gmail.com>
2019-04-06 10:05:15 +0200

    camera works

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import {View} from 'react-native';
+import {View, StatusBar} from 'react-native';
 import Camera from './Camera';
 
 export default () => (
-	<View style={{marginTop: 50}}>
+	<View style={{flex: 1}}>
+		<StatusBar backgroundColor="white" barStyle="light-content" />
 		<Camera />
 	</View>
 );
--- src/Camera.tsx
+++ src/Camera.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {Camera, Permissions} from 'expo';
 
-class Camera extends React.Component {
+class CameraView extends React.Component {
+	state = {
+		type: Camera.Constants.Type.back,
+		hasCameraPermission: null
+	};
+	async componentDidMount() {
+		const {status} = await Permissions.askAsync(Permissions.CAMERA);
+		this.setState({hasCameraPermission: status === 'granted'});
+	}
 	render() {
 		return (
-			<View>
-				<Text>put camera here</Text>
+			<View style={{flex: 1}}>
+				<Camera style={{flex: 1}} type={this.state.type}>
+					<View
+						style={{
+							flex: 1,
+							backgroundColor: 'transparent',
+							flexDirection: 'row'
+						}}
+					/>
+				</Camera>
 			</View>
 		);
 	}
 }
 
-export default Camera;
+export default CameraView;
