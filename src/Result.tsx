13d6e06821cad30505c1cff8741489325a579d35
Jonny Burger <jonathanburger11@gmail.com>
2019-04-06 11:44:56 +0200

    hammer time

--- src/Result.tsx
+++ src/Result.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import {View, Dimensions, TouchableWithoutFeedback} from 'react-native';
+import {
+	View,
+	Dimensions,
+	TouchableWithoutFeedback,
+	TouchableOpacity
+} from 'react-native';
 import BottomSheet from 'reanimated-bottom-sheet';
+import {CancelButton} from './CancelButton';
 
 const screenHeight = Dimensions.get('window').height;
 const previewHeight = 200;
@@ -9,7 +15,7 @@ export class Results extends React.Component {
 	ref: React.Ref<BottomSheet> | null = null;
 	renderInner = () => {
 		return (
-			<TouchableWithoutFeedback onPress={() => this.ref.snapTo(1)}>
+			<TouchableWithoutFeedback onPress={() => {}}>
 				<View
 					style={{
 						height: screenHeight - previewHeight,
@@ -23,11 +29,21 @@ export class Results extends React.Component {
 		return (
 			<TouchableWithoutFeedback
 				onPress={() => {
+					// @ts-ignore
 					this.ref.snapTo(0);
 				}}
 				style={{height: previewHeight}}
 			>
-				<View style={{height: previewHeight, backgroundColor: 'white'}} />
+				<View style={{height: previewHeight, backgroundColor: 'white'}}>
+					<TouchableOpacity
+						style={{position: 'absolute', right: 0, top: 0, padding: 10}}
+						onPress={() => {
+							this.ref.snapTo(2);
+						}}
+					>
+						<CancelButton />
+					</TouchableOpacity>
+				</View>
 			</TouchableWithoutFeedback>
 		);
 	};
--- src/colors.ts
+++ src/colors.ts
@@ -1,2 +1,5 @@
+import {darken} from 'polished';
+
 export const SULZER = '#1D59AF';
 export const WINTI = '#EA0001';
+export const GRAY = darken(0.07, '#fff');
