75991de88a15bcc2fd2c903597230156340ee763
Jonny Burger <jonathanburger11@gmail.com>
2019-04-07 10:26:42 +0200

    Revert "no reverse"
    This reverts commit b6ff1b82072baafc1aef0813bbbb2ed268f567d2.

--- src/Explore.tsx
+++ src/Explore.tsx
@@ -116,7 +116,7 @@ class Explore extends React.Component<Props> {
             </LoadingWrapper>,
             p => (
               <FlatList
-                data={p}
+                data={p.reverse()}
                 keyExtractor={(item, index) => String(index)}
                 renderItem={({ item }) => this.renderPostItem(item)}
               />
